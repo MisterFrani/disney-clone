b831e0f6226fbe7637f7e9b34535f8b8612e7b1b
frani <bizifrani@icloud.com>
2024-05-24 14:15:05 +0200

    feat: Update API key handling in CallApi.tsx

--- src/services/CallApi.tsx
+++ src/services/CallApi.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
+// Dans votre fichier CallApi.tsx
 
 const movieBaseUrl = "https://api.themoviedb.org/3";
-const apiKey = process.env.REACT_APP_API_KEY;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const getVideos = axios.get(
   `${movieBaseUrl}/trending/all/day?api_key=${apiKey}`
